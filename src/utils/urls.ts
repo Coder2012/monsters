aa895296b44935455e1dfeabb2ea168b37cb4434
Neil Brown <neilbrown08@hotmail.com>
2022-09-02 14:08:14 +0100

    switch to render.com

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,2 +1,3 @@
 // export const api = 'http://localhost:3001';
-export const api = 'https://monsters-rewards.herokuapp.com/';
+// export const api = 'https://monsters-rewards.herokuapp.com/';
+export const api = 'https://monsters-backend.onrender.com//';
