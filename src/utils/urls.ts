1776e12f096f21f17202ae8b5b8d63d405bd8178
Neil Brown <neilbrown08@hotmail.com>
2022-09-02 18:27:48 +0100

    test github pr

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,3 +1,4 @@
 // export const api = 'http://localhost:3001';
+// Heroku is being deprecated
 // export const api = 'https://monsters-rewards.herokuapp.com/';
 export const api = 'https://monsters-backend.onrender.com/';
